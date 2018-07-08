9eb49a07e0f068d1320e9f930094acc315b0f4ec
Chenna Maduru <maduru.chenna123@gmail.com>
2018-07-08 19:53:27 +0530

    updated

---
+++ BankApp/src/LoanAccount2.java
@@ -0,0 +1,13 @@
+package com.citi.store;
+i
+public class LoanAccount
+{
+	private int id;
+	public LoanAccount(int accNo,double accBalance,int id)
+	{
+		this.accNo=accNo;
+		this.accBalance=accBalance;
+		this.id=id;
+	}
+
+}
